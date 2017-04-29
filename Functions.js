4aacf9d684377e4352b5cdac6199af800a1d63df
Jake Hamel <jakehamelhs@gmail.com>
2017-04-29 13:48:37 -0500

    Adds a map, then breaks it!

--- Functions.js
+++ Functions.js
@@ -114,12 +114,45 @@ function randRestaurant() {
 
     var selectedRestaurant = restaurants[Math.floor(Math.random() * restaurants.length)];
     document.getElementById("rest").innerHTML = selectedRestaurant;
+    this.updateMap();
 }
 
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-    });
+  var uluru = {lat: 43.074761, lng: -89.391817};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: uluru
+  });
+
+  var marker = new google.maps.Marker({
+    title: document.getElementById("rest").textContent,
+    position: uluru,
+    setMap: this.map
+  });
 }
 
+function updateMap() {
+  var map = new google.maps.Map(document.getElementById('map'))
+
+  geocoder = new google.maps.Geocoder();
+  var address = document.getElementById("rest").textContent;
+  var position = geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var lat = results[0].geometry.location.lat();
+      var lng = results[0].geometry.location.lng();
+      var position = new google.maps.LatLng(lat, lng);
+      return position;
+    }
+    else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+    alert(position);
+  });
+
+  var marker = new google.maps.Marker({
+    title: document.getElementById("rest").textContent,
+    position: position,
+    setMap: this.map
+  })
+}
